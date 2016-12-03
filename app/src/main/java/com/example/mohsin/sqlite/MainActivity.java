6b0da03a580bca59244f72e0b63df9478999037c
ahmedraza <ahmedraza@yahoo.com>
2016-12-04 01:15:08 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mohsin/sqlite/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.mohsin.sqlite;
+
+import android.app.AlertDialog;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    DatabaseHelper databaseHelper;
+    EditText editName,editSurname,editMarks ,editTextId;
+    Button btnAddData;
+    Button btnviewAll;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        editName = (EditText)findViewById(R.id.editText_name);
+        editSurname = (EditText)findViewById(R.id.editText_surname);
+        editMarks = (EditText)findViewById(R.id.editText_Marks);
+        editTextId = (EditText)findViewById(R.id.editText_id);
+        btnAddData = (Button)findViewById(R.id.button_add);
+        btnviewAll = (Button)findViewById(R.id.button_viewAll);
+        AddData();
+        viewAll();
+
+    }
+
+    public  void AddData() {
+        btnAddData.setOnClickListener(
+                new   View .OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = databaseHelper.insertData(editName.getText().toString(),
+                                editSurname.getText().toString(),
+                                editMarks.getText().toString() );
+                        if(isInserted == true)
+                            Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this,"Data not Inserted",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        btnviewAll.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = databaseHelper.getAllData();
+                        if(res.getCount() == 0) {
+                            // show message
+                            showMessage("Error","Nothing found");
+                            return;
+                        }
+
+                        StringBuffer buffer = new StringBuffer();
+                        while (res.moveToNext()) {
+                            buffer.append("Id :"+ res.getString(0)+"\n");
+                            buffer.append("Name :"+ res.getString(1)+"\n");
+                            buffer.append("Surname :"+ res.getString(2)+"\n");
+                            buffer.append("Marks :"+ res.getString(3)+"\n\n");
+                        }
+
+                        // Show all data
+                        showMessage("Data",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title,String Message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+}
